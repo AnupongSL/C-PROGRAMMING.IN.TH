a442396994cdcd7a8c6e784530c4d9c31c870c14
Anupog <anupongsutlek022@gmail.com>
2022-10-30 18:31:18 +0700

    0004

---
+++ [0004] Character Checker.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+    int i, sum, cu = 0;
+    char S[10000];
+    scanf("%s", S);
+    sum = strlen(S);
+    for (i = 0; i < sum; i++)
+    {
+        if (isupper(S[i]))
+            cu++;
+    }
+    if (cu == sum)
+    {
+        printf("All Capital Letter");
+    }
+    else if (cu == 0)
+    {
+        printf("All Small Letter");
+    }
+    else
+        printf("Mix");
+    return 0;
+}
